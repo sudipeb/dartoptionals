9e1e108d4bab19ab453fbf4bc0c80838e36c3a30
ebpearls <ebpearls@Mac-mini.local>
2025-09-17 08:49:38 +0545

    initialized the empty repo

---
+++ bin/dartoptionals.dart
@@ -0,0 +1,5 @@
+import 'package:dartoptionals/dartoptionals.dart' as dartoptionals;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dartoptionals.calculate()}!');
+}
---
+++ test/dartoptionals_test.dart
@@ -0,0 +1,8 @@
+import 'package:dartoptionals/dartoptionals.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
