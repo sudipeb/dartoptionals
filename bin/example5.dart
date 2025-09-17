5bd643c7477746f66167ecbcd2ba7b53dda6e689
ebpearls <ebpearls@Mac-mini.local>
2025-09-17 14:24:01 +0545

    nullable operators

---
+++ bin/example5.dart
@@ -0,0 +1,11 @@
+void main(){
+  String? name;
+   print(name ?? 'sudip');
+
+  String? surname;
+  final bla = surname??name; // optional string data type as the input source is from the 
+
+  String? college;
+  print(name ?? 'kathford');
+  print(college ?? name);
+}
